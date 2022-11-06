6cb3caf9734a20394de7124eb0a37768b43ad117
Denis <marksdev@gmail.com>
2022-11-06 16:22:30 -0300

    chore: webpack production container and marketing

--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -1,5 +1,5 @@
 const { merge } = require("webpack-merge");
-const HtmlWebpackPuglin = require("html-webpack-plugin");
+
 const commonConfig = require("./webpack.common");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const packageJSon = require("../package.json");
@@ -20,10 +20,7 @@ const devConfig = {
         "./MarketingApp": "./src/bootstrap",
       },
       shared: packageJSon.dependencies,
-    }),
-    new HtmlWebpackPuglin({
-      template: "./public/index.html",
-    }),
+    })
   ],
 };
 
